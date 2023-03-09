47547c7d86b995fc1fd32c41ce249e7360016f2b
Will Harney <wharney@salesforce.com>
2023-03-09 14:54:34 -0500

    Convert validators to type predicates.

--- lib/validators.ts
+++ lib/validators.ts
@@ -6,7 +6,7 @@ MIT License
 
 Copyright (c) 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019 Phil Booth
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
+Permission is hereby granted, free of charge, to unknown person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
@@ -16,10 +16,10 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF unknown KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR unknown CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
@@ -28,39 +28,39 @@ SOFTWARE.
 "use strict";
 
 /* Validation functions copied from check-types package - https://www.npmjs.com/package/check-types */
-export function isFunction(data: any): boolean {
+export function isFunction(data: unknown): data is (...args: unknown[]) => unknown {
   return typeof data === 'function';
 }
 
-export function isNonEmptyString(data: any): boolean {
+export function isNonEmptyString(data: unknown): data is string {
   return isString(data) && data !== '';
 }
 
-export function isDate(data: any): boolean {
+export function isDate(data: unknown): data is Date {
   return isInstanceStrict(data, Date) && isInteger(data.getTime());
 }
 
-export function isEmptyString(data: any): boolean {
+export function isEmptyString(data: unknown): data is '' {
   return data === '' || (data instanceof String && data.toString() === '');
 }
 
-export function isString(data: any): boolean {
+export function isString(data: unknown): data is string {
   return typeof data === 'string' || data instanceof String
 }
 
-export function isObject(data: any): boolean {
+export function isObject(data: unknown): data is object {
   return toString.call(data) === '[object Object]';
 }
 
-export function isInstanceStrict(data: any, prototype: Function): boolean {
+export function isInstanceStrict<T extends Function>(data: unknown, Constructor: T): data is T['prototype'] {
   try {
-    return data instanceof prototype;
-  } catch (error) {
+    return data instanceof Constructor;
+  } catch {
     return false;
   }
 }
 
-export function isInteger(data: any): boolean {
+export function isInteger(data: unknown): data is number {
   return typeof data === 'number' && data % 1 === 0;
 }
 /* End validation functions */
